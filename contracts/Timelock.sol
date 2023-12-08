7044afbae7491097a31efa853e9fee186760484b
FireDAO <114811457+FireDAOlab@users.noreply.github.com>
2023-12-08 18:17:11 +0800

    Update Timelock.sol

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -105,8 +105,4 @@ contract Timelock {
         return returnData;
     }
 
-    function getBlockTimestamp() internal view returns (uint) {
-        // solium-disable-next-line security/no-block-members
-        return block.timestamp;
-    }
-}
+
